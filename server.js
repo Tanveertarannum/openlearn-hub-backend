7272db56bebc9e8037daebe6f5083ec63ac61b41
Syeda Umaima <syedaumaima07@gmail.com>
2025-05-31 00:27:14 +0530

    fixed bugs part 2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const admin = require("firebase-admin");
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = require("node-fetch");
 
 //Debugging: Ensure environment variables are loaded
 console.log("Checking Environment Variables...");
